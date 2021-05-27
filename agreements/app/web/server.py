628033ea304f3f99e09ffcb2015117eb16b851da
lukvmil <millerluke1364@gmail.com>
2021-05-27 14:05:44 -0400

    adding subpages and flask routing

--- agreements/app/web/server.py
+++ agreements/app/web/server.py
@@ -12,4 +12,12 @@ def get_html(name):
 def home():
     return get_html('home')
 
+@flask_app.route('/about')
+def about():
+    return get_html('about')
+
+@flask_app.route('/help')
+def help():
+    return get_html('help')
+
 # flask_app.run(host="127.0.0.1", port=80, debug=True)
