eac74b0271e116d467ea3ba79eae60e85a589016
lukvmil <millerluke1364@gmail.com>
2021-05-28 13:53:37 -0400

    general improvements to website, and added api call to server

--- agreements/app/web/server.py
+++ agreements/app/web/server.py
@@ -1,9 +1,11 @@
+import json
+
 from flask import Flask, redirect
 
 flask_app = Flask(__name__)
 
 def get_html(name):
-    f = open(f'app/web/{name}.html')
+    f = open(f'app/web/{name}.html', 'r')
     html = f.read()
     f.close()
     return html
@@ -23,5 +25,14 @@ def about():
 @flask_app.route('/help')
 def help():
     return get_html('help')
+
+@flask_app.route('/api/latest_agreements')
+def latest_agreements():
+    f = open('app/database/db.json', 'r')
+    db = json.load(f)
+    f.close()
+    statuses = list(db['agreements'].keys())[1:11]
+
+    return statuses        
 
 # flask_app.run(host="127.0.0.1", port=80, debug=True)
