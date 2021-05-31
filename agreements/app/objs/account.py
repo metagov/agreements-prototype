4d617f8ce8d8c2cb21808e4428dabe1784eeb18f
lukvmil <millerluke1364@gmail.com>
2021-05-31 16:31:26 +0000

    updated welcome message

--- agreements/app/objs/account.py
+++ agreements/app/objs/account.py
@@ -36,7 +36,7 @@ class Account:
         # greeting message for new users (excluding the agreement engine)
         if new_user and (self.id != core.api.me().id):
             self.logger.info(f"Welcoming {self.screen_name}")
-            message = f"@{self.screen_name} Welcome to agreement engine! Here are some useful links to get you started: http://agreements.metagov.org"
+            message = f"@{self.screen_name} Welcome to Agreement Engine! Check out http://agreements.metagov.org/about and http://agreements.metagov.org/help to learn about agreements and how to make them!"
             core.emit(message)
 
     # returns dict from db
